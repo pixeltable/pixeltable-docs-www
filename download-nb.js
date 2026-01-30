df161785be24881c7b59c4987a2ef135e15e0825
Pierre Brunelle <pierre@noteable.io>
2026-01-29 16:46:09 -0800

    Deploy documentation v0.5.16.dev8.1be5fcd3 from 1be5fcd3

--- download-nb.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.download-notebook').forEach(link => {
-    link.addEventListener('click', async (e) => {
-        e.preventDefault();
-        const url = link.href;
-        const filename = link.dataset.filename;
-        await downloadNotebook(url, filename);
-    });
-});
